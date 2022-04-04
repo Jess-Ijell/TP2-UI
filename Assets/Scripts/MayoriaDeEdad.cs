3cab8b23739d413bd57e11fe5fb380d40b3867a2
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-04-04 12:13:54 -0300

    videos 1 to 4 finished

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadusuario;
+    public Text mitexto;
+
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            mitexto.text = "Es mayor de edad.";
+            Debug.Log("Es mayor de edad.");
+        }
+
+        else
+        {
+            mitexto.text = "No es mayor de edad.";
+            Debug.Log("No es mayor de edad.");
+        }
+    }
+}
