326ce33741d3dd859f05abd5a384c3d837dd666b
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-04-19 11:51:08 -0300

    TP2-UI finished

--- Assets/Scripts/ValidarContraseña.cs
+++ Assets/Scripts/ValidarContraseña.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContraseña : MonoBehaviour
 {
+
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,7 +27,20 @@ public class ValidarContraseña : MonoBehaviour
 
     public void ValidarContrasenia()
     {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+        }
 
+        else
+        {
+            Debug.Log("Contraseña Incorrecta");
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
+        }
     }
 
 }
